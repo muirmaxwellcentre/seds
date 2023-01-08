9f5fdc973615bb3d63e3081017be55583e87d24a
Martyna <34917508+martynakrysinska@users.noreply.github.com>
2023-01-08 12:33:41 +0100

    Updates

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import {createApp} from 'vue';
-import App from './App.vue';
-import BootstrapVue3 from 'bootstrap-vue-3';
-import './assets/main.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
-createApp(App).use(BootstrapVue3).mount('#app');
